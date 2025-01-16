dcf3a236527c2c602ac8fc37e34ca6621904ab80
priyanshusingh-07 <priyanshu7606@gmail.com>
2025-01-16 21:31:07 +0530

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,20 @@
+let change=document.getElementsByClassName('outer')
+
+function red(){
+   document.getElementById("box").style.backgroundColor="red"  
+}
+function blue(){
+    document.getElementById("box").style.backgroundColor="blue"  
+ }
+ function green(){
+    document.getElementById("box").style.backgroundColor="green"  
+ }
+ function black(){
+    document.getElementById("box").style.backgroundColor="black"  
+ }
+ function purple(){
+    document.getElementById("box").style.backgroundColor="purple"  
+ }
+ function pink(){
+    document.getElementById("box").style.backgroundColor="pink"  
+ }
